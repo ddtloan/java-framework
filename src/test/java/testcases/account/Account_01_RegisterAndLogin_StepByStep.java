cd3a872147f1e804e72400bc20301d625e5f5512
ddtloan <trucloan.hht@gmail.com>
2020-04-02 18:50:40 +0700

    first commit

---
+++ src/test/java/testcases/account/Account_01_RegisterAndLogin_StepByStep.java
@@ -0,0 +1,22 @@
+package testcases.account;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Account_01_RegisterAndLogin_StepByStep {
+    @BeforeClass
+    public void beforeClass() {
+
+    }
+
+    @Test
+    public void TC_01_RegisterT() {
+
+    }
+
+    @AfterClass
+    public void afterClass() {
+
+    }
+}
